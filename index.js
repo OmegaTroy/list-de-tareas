b2fcdc0a8c42e6112b4c1eed5f5c21a49009dcf6
OmegaTroy <juli.mjoi44@gmail.com>
2022-07-23 18:37:06 -0300

    actualizacion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () =>{
-let title = document.getElementById('titulo');
-let table = document.getElementById('tabla');
+const $ = selector => document.querySelector(selector);
+let $title = $('#titulo');
+let $table = $('#tabla');
 let id = 1;
 
 
@@ -10,54 +11,55 @@ const removeElement = (id) => {
     console.log(id)
 }
 
-    title.addEventListener('keypress',(e)=>{
+    $title.addEventListener('keypress',(e)=>{
+        const {value} = $title;
         if (e.key === "Enter"){
             e.preventDefault();
-            if(title.value == ''){
-                alert('escribe una tarea')
+            if(!value){
+                return alert('escribe una tarea')
             }else{
-                const row = table.insertRow();
-                row.setAttribute('id',id++);
-                row.classList.add('list__tr');
-                row.innerHTML = `
+                const $row = $table.insertRow();
+                $row.setAttribute('id',id++);
+                $row.classList.add('list__tr');
+                $row.innerHTML = `
                 <td></td>
                 <td></td>
                 `;
-                const input = document.createElement('input');
-                input.classList.add('list__item');
-                row.children[0].appendChild(input);
-                input.setAttribute('value',`${title.value}`);
-                input.setAttribute('maxlength', 20)
-                input.setAttribute('disabled',true);
-                input.addEventListener('keypress',(e) =>{
+                const $input = document.createElement('input');
+                $input.classList.add('list__item');
+                $row.children[0].appendChild($input);
+                $input.setAttribute('value',`${value}`);
+                $input.setAttribute('maxlength', 20)
+                $input.setAttribute('disabled',true);
+                $input.addEventListener('keypress',(e) =>{
                     if(e.key === "Enter"){
-                        input.setAttribute('disabled',true);
+                        $input.setAttribute('disabled',true);
                     }
                 } )
                 
-                row.children[0]
-                row.addEventListener('dblclick',()=>{
-                    row.getAttribute('id');
-                    input.classList.add('list__complete');
+                $row.children[0]
+                $row.addEventListener('dblclick',()=>{
+                    $row.getAttribute('id');
+                    $input.classList.add('list__complete');
                 });
                 
-                const editBtn = document.createElement('button');
-                editBtn.classList.add('editBtn');
-                editBtn.innerHTML = '<i class="fa-solid fa-pencil"></i>';
-                row.children[1].appendChild(editBtn);
-                editBtn.addEventListener('click',()=>{
-                    input.removeAttribute('disabled');
-                    input.focus();
+                const $editBtn = document.createElement('button');
+                $editBtn.classList.add('editBtn');
+                $editBtn.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+                $row.children[1].appendChild($editBtn);
+                $editBtn.addEventListener('click',()=>{
+                    $input.removeAttribute('disabled');
+                    $input.focus();
                 });
                 
-                const removeBtn = document.createElement('button');
-                removeBtn.classList.add('btn');
-                removeBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-                row.children[1].appendChild(removeBtn);
-                removeBtn.addEventListener('click',()=>{
-                    removeElement(row.getAttribute('id'))
+                const $removeBtn = document.createElement('button');
+                $removeBtn.classList.add('btn');
+                $removeBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+                $row.children[1].appendChild($removeBtn);
+                $removeBtn.addEventListener('click',()=>{
+                    removeElement($row.getAttribute('id'))
                 });
-                document.getElementById("titulo").value = "";
+                $("#titulo").value = "";
             }
         }
     });
